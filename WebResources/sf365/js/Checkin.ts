a0d57ac4b1683afd720c37f57ac2b56a16b2ee49
Jared Howard <JaredH@eohmc.onmicrosoft.com>
2020-09-13 11:59:18 +0200

    Initial Commit Web Resources

---
+++ WebResources/sf365/js/Checkin.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../node_modules/@types/knockout/index.d.ts" />
+/// <reference path="../../node_modules/knockout/build/output/knockout-latest.js" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+namespace sf365.checkin
+{
+
+    class CheckInViewModel
+    {
+        isbusy: KnockoutObservable<boolean>;
+        constructor()
+        {
+            this.isbusy = ko.observable(false);
+
+
+        }
+        public foo()
+        {
+            alert("bar");
+        }
+    }
+
+}
